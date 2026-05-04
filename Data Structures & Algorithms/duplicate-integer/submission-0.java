679e14ea023296adba69d43cddf2183fd3635976
Tanisha Gupta <81873412+tanishagupta20@users.noreply.github.com>
2026-05-04 19:15:27 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(hm.containsKey(nums[i])) return true;
+            else hm.put(nums[i], 1);
+        }
+        return false;
+    }
+}
---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int secNum = target - nums[i];
+            if(hm.containsKey(secNum)) return new int[]{hm.get(secNum), i};
+            hm.put(nums[i], i);
+        }
+        return new int[]{0,0};
+    }
+}
